8240075b1dd8a0279897e08c1f5ae9ac04f20444
Tung Luu <38417805+tunglm2203@users.noreply.github.com>
2019-12-08 15:29:33 +0900

    Update install_ros.sh, update new key

--- install_ros.sh
+++ install_ros.sh
@@ -8,7 +8,7 @@
 # Setup your sources.list
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu xenial main" > /etc/apt/sources.list.d/ros-latest.list'
 # Setup your keys
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 # Update to Latest Software Lists
 sudo apt-get update
 # Install ROS Kinetic Desktop Full
