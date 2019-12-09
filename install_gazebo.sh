950548641507611a36017df9a9562fd8381c6a72
Tung Luu <38417805+tunglm2203@users.noreply.github.com>
2019-12-09 14:18:34 +0900

    Update source to install gazebo latest version

--- install_gazebo.sh
+++ install_gazebo.sh
@@ -4,6 +4,12 @@
 # If you have any problem, please visit above page for more information
 # NOTE: Run this file without root permission (do not use 'sudo')
 
+sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
+wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install gazebo7
+sudo apt-get install libgazebo7-dev
+
 # Installing the following software packages
 sudo apt-get install gazebo7 ros-kinetic-qt-build ros-kinetic-gazebo-ros-control ros-kinetic-gazebo-ros-pkgs ros-kinetic-ros-control ros-kinetic-control-toolbox ros-kinetic-realtime-tools ros-kinetic-ros-controllers ros-kinetic-xacro python-wstool ros-kinetic-tf-conversions ros-kinetic-kdl-parser ros-kinetic-sns-ik-lib
 
